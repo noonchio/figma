87428997e7ecce3ff135dd35d0f6cbb763164e80
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2023-05-27 15:10:01 -0500

    plugin tutorial updates

--- code.ts
+++ code.ts
@@ -1,3 +1,17 @@
 figma.showUI(__html__);
+figma.ui.resize(500, 500);
 
-figma.ui.resize(500,500);
+figma.ui.onmessage = pluginMessage => {
+  const postComponentSet = figma.root.findOne(node => node.type === "COMPONENT_SET" && node.name === "post") as ComponentSetNode;
+  const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
+  const defaultDark = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
+
+
+  if (pluginMessage.darkModeState === true) {
+        defaultDark.createInstance();
+  } else {
+        defaultVariant.createInstance();
+  }
+
+  figma.closePlugin();
+};
